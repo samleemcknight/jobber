a1959f046e4e8cba62bf1ab96a4f73f256802767
3ddy925 <3ddy70@gmail.com>
2021-03-15 16:57:39 -0700

    adds accounts/signup/ and templates for login and signup and views for signup

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # temporary for 'sanity check':
 from django.http import HttpResponse
@@ -7,3 +9,17 @@ from django.http import HttpResponse
 # home view:
 def home(request):
   return render(request, 'index.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
