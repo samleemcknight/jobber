e75e9edaa6d20c3b5f5b706e998803ae5b1c2406
samleemcknight <samleemcknight@gmail.com>
2021-03-15 16:53:02 -0600

    sets up main app home view

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+# temporary for 'sanity check':
+from django.http import HttpResponse
 # Create your views here.
+
+# home view:
+def home(request):
+  return render(request, 'index.html')
