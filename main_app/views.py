d966b46115200478b150c0c9dff970e2b5e601d0
3ddy925 <3ddy70@gmail.com>
2021-03-16 11:32:04 -0700

    displays event onto homepage onto index.html

--- main_app/views.py
+++ main_app/views.py
@@ -4,11 +4,17 @@ from django.contrib.auth import login
 
 from .forms import SignupForm, EventForm
 
+from .models import Event
+
 # Create your views here.
 
 # home view:
 def home(request):
-  return render(request, 'index.html')
+  events = Event.objects.all()
+  context = {
+    'events': events
+  }
+  return render(request, 'index.html', context)
 
 def signup(request):
   error_message = ''
