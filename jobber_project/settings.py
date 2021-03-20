651efc1f0d4a713cf722ca8165ebd888dbcd051c
samleemcknight <samleemcknight@gmail.com>
2021-03-19 18:43:56 -0600

    replaces env variables

--- jobber_project/settings.py
+++ jobber_project/settings.py
@@ -141,9 +141,9 @@ LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'projectjobber@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 import django_heroku
