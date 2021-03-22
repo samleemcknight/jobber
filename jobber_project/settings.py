f74ab3dfa1688a970eee61e9be2f03a4a9815e19
samleemcknight <samleemcknight@gmail.com>
2021-03-22 15:36:26 -0600

    resets debug for github repo

--- jobber_project/settings.py
+++ jobber_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'eme#fxc+j7lj6qpt2ag+1d_od9pdt4e*ql$mv_ov8s+)+43+7c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
